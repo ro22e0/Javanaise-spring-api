80e2047e5d2e109bea56a923d8459533c67a01b2
Bajazet Ronaël <ronael.bajazet@epitech.eu>
2016-04-29 13:20:46 +0200

    update endpoints

--- src/main/java/com/javanaise/ws/controllers/ApiController.java
+++ src/main/java/com/javanaise/ws/controllers/ApiController.java
@@ -12,24 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
 public class ApiController {
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> infos() {
         ResponseEntity<String> errorResponseEntity =
-                new ResponseEntity<String>("Mapped \"{[/users/sign_up],methods=[POST],produces=[application/json]}\"\n" +
-                        "Mapped \"{[/users/sign_in],methods=[POST],produces=[application/json]}\"\n" +
-                        "Mapped \"{[/users/{userId}],methods=[GET],produces=[application/json]}\"\n" +
-                        "Mapped \"{[/users],methods=[GET],produces=[application/json]}\"\n" +
-                        "Mapped \"{[/users],methods=[PUT],produces=[application/json]}\"\n" +
-                        "Mapped \"{[/webapi/rss/create],methods=[POST]}\"\n" +
-                        "Mapped \"{[/error],produces=[text/html]}\"\n" +
-                        "Mapped \"{[/error]}\"\n", HttpStatus.BAD_REQUEST);
+                new ResponseEntity<String>("Mapped \"[/users/sign_up], methods=[POST], produces=[application/json, value=[email, password]\"\n" +
+                        "Mapped \"[/users/sign_in], methods=[POST], produces=[application/json], value=[email, password]\"\n" +
+                        "Mapped \"[/users/{userId}], methods=[GET], produces=[application/json]\"\n" +
+                        "Mapped \"[/users],methods=[GET], produces=[application/json]\"\n" +
+                        "Mapped \"[/users],methods=[PUT], produces=[application/json], value=[firstname, lastname, email, password]\"\n", HttpStatus.OK);
         return errorResponseEntity;
     }
 
-    @RequestMapping(value = "/error", method = RequestMethod.GET)
+    @RequestMapping(value = "/error")
     public ResponseEntity<?> error() {
         ResponseEntity<String> errorResponseEntity =
                 new ResponseEntity<String>("No route found", HttpStatus.BAD_REQUEST);
