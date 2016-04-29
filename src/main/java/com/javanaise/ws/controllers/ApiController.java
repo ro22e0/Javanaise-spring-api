7cd793b5ad0c4a6c72781352ea7c6bcd64011ff5
Bajazet Ronaël <ronael.bajazet@epitech.eu>
2016-04-29 16:45:53 +0200

    add feed parser

--- src/main/java/com/javanaise/ws/controllers/ApiController.java
+++ src/main/java/com/javanaise/ws/controllers/ApiController.java
@@ -22,14 +22,8 @@ public class ApiController {
                         "Mapped \"[/users/sign_in], methods=[POST], produces=[application/json], value=[email, password]\"\n" +
                         "Mapped \"[/users/{userId}], methods=[GET], produces=[application/json]\"\n" +
                         "Mapped \"[/users],methods=[GET], produces=[application/json]\"\n" +
-                        "Mapped \"[/users],methods=[PUT], produces=[application/json], value=[firstname, lastname, email, password]\"\n", HttpStatus.OK);
-        return errorResponseEntity;
-    }
-
-    @RequestMapping(value = "/error")
-    public ResponseEntity<?> error() {
-        ResponseEntity<String> errorResponseEntity =
-                new ResponseEntity<String>("No route found", HttpStatus.BAD_REQUEST);
+                        "Mapped \"[/users],methods=[PUT], produces=[application/json], value=[firstname, lastname, email, password]\"\n" +
+                        "Mapped \"[/feeds/create], methods=[POST], produces=[application/json], value=[url]\"", HttpStatus.OK);
         return errorResponseEntity;
     }
 }
--- src/main/java/com/javanaise/ws/controllers/FeedController.java
+++ src/main/java/com/javanaise/ws/controllers/FeedController.java
@@ -1,8 +1,18 @@
 package com.javanaise.ws.controllers;
 
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URL;
+import java.util.Map;
 
 /**
  * Created by ro22e0 on 29/04/2016.
@@ -12,4 +22,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/feeds", produces = MediaType.APPLICATION_JSON_VALUE)
 public class FeedController {
 
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public ResponseEntity<?> create(@RequestBody Map<String, String> params) {
+
+        try {
+            URL feedUrl = new URL(params.get("url"));
+
+            SyndFeedInput input = new SyndFeedInput();
+            SyndFeed feed = input.build(new XmlReader(feedUrl));
+        } catch (Exception ex) {
+            ResponseEntity<String> errorResponseEntity =
+                    new ResponseEntity<String>("{\"error\": " + ex.getMessage() + "}", HttpStatus.BAD_REQUEST);
+            return errorResponseEntity;
+        }
+
+        ResponseEntity<String> errorResponseEntity =
+                new ResponseEntity<String>("{\"error\": \"Not found\"}", HttpStatus.NOT_FOUND);
+        return errorResponseEntity;
+    }
 }
